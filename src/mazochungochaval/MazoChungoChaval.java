b91967f7630ff6290028ff8bb5a9080a28daa6f1
luis- <luis-@DESKTOP-5VG1NHG>
2018-12-12 20:29:33 +0100

    "Inicio del proyexto mazo chungo"

---
+++ src/mazochungochaval/MazoChungoChaval.java
@@ -0,0 +1,19 @@
+
+package mazochungochaval;
+
+
+public class MazoChungoChaval {
+
+   
+    public static void main(String[] args) {
+        
+        
+        
+        
+        //Esto es mazo importante loco
+        
+        
+        
+    }
+    
+}
